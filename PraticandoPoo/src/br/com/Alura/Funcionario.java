e390492b5d087e4e1b660987251997d3e0bcf773
Lucas Silva <lucas.silva.s1998@gmail.com>
2025-12-22 15:26:05 -0300

    updates

---
+++ PraticandoPoo/src/br/com/Alura/Funcionario.java
@@ -0,0 +1,8 @@
+package br.com.Alura;
+
+public class Funcionario {
+
+    String nome;
+    String cargo;
+    double salario;
+}
---
+++ PraticandoPoo/src/br/com/Alura/Main.java
@@ -0,0 +1,19 @@
+package br.com.Alura;
+
+public class Main {
+    static void main(String[] args) {
+
+        Funcionario funcionario_01 = new Funcionario();
+
+        funcionario_01.nome = "Lucas";
+        funcionario_01.cargo = "Engenheiro de Software Sênior";
+        funcionario_01.salario = 15539;
+
+        Funcionario funcionario_02 = new Funcionario();
+
+        funcionario_02.nome = "Matheus";
+        funcionario_02.cargo = "Gerente de Projetos";
+        funcionario_02.salario = 10879;
+        
+    }
+}
