24ab9c9775c0b006685e0f4476416d4bd0cfc35a
Lucas Silva <lucas.silva.s1998@gmail.com>
2026-01-22 16:58:43 -0300

    Update Main.java

--- PraticandoColecoesEStreams/FiltrosDeListas/src/Main.java
+++ PraticandoColecoesEStreams/FiltrosDeListas/src/Main.java
@@ -4,13 +4,19 @@ import java.util.stream.Collectors;
 public class Main {
     static void main(String[] args) {
 
-        List<String> funcionarios = List.of("Anna", "Bruno", "Carlos", "Amanda", "Alice", "Daniel", "Caroline");
+        List<String> funcionarios = List.of("Anna", "Bruno", "Carlos", "Amanda", "Alice", "Daniel", "Caroline", "Bartolomeu", "Ricardo");
 
         List<String> funcionariosComNomesCurtos =  funcionarios.stream()
                 .filter(f -> f.length() <= 5)
                 .collect(Collectors.toList());
 
-        System.out.println(funcionariosComNomesCurtos);
+        List<String> funcionariosComNomesLongos =  funcionarios.stream()
+                .filter(f -> f.length() >= 7)
+                .collect(Collectors.toList());
+
+
+        System.out.println("Funcionários com nomes curtos: " +funcionariosComNomesCurtos);
+        System.out.println("Funcionários com nomes longos: " +funcionariosComNomesLongos);
 
 
     }
